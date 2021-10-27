805a4efd5495c0cf59b50dd5a13b2ba339df4b9e
Franklin Chieze <chieze.franklin@gmail.com>
2021-10-27 02:40:16 +0100

    add small text to bottom of components

--- src/canvas.tsx
+++ src/canvas.tsx
@@ -193,23 +193,26 @@ const Canvas = (prop: CanvasPropType) => {
                                             data: {
                                                 label: (
                                                   <>
-                                                    {action.image && (<img src={action.image} width="98" style={{pointerEvents: "none"}} />)}
+                                                    {action.image && (<img src={action.image} width="98" height="98" style={{pointerEvents: "none"}} />)}
                                                     {action.paths && action.paths.length && (
-                                                        <svg version="1.1" xmlns="http://www.w3.org/2000/svg" width="100" height="100" viewBox="0 0 100 100" style={{pointerEvents: "none"}}>
-                                                            {action.paths.map((path, index) => (
-                                                                <path
-                                                                    key={index}
-                                                                    d={path.d}
-                                                                    fill={path.fill || "black"}
-                                                                    stroke={path.stroke || "black"}
-                                                                    transform={path.transform}
-                                                                    // style={CssString(path.style || "")} // TODO: catch malformed strings
-                                                                />
-                                                            ))}
+                                                        <svg version="1.1" xmlns="http://www.w3.org/2000/svg" width="98" height="98" viewBox="0 0 98 98" style={{pointerEvents: "none"}}>
+                                                            <g transform="scale(0.98)">
+                                                                {action.paths.map((path, index) => (
+                                                                    <path
+                                                                        key={index}
+                                                                        d={path.d}
+                                                                        fill={path.fill || "black"}
+                                                                        stroke={path.stroke || "black"}
+                                                                        transform={path.transform}
+                                                                        // style={CssString(path.style || "")} // TODO: catch malformed strings
+                                                                    />
+                                                                ))}
+                                                            </g>
                                                         </svg>
                                                     )}
                                                   </>
                                                 ),
+                                                alt: action.text,
                                               },
                                         }
                                     ]
