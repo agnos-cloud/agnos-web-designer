9230861896abb35229946ec1edd900a3f7b665a7
chieze-franklin <chieze.franklin@gmail.com>
2022-03-26 00:12:49 +0100

    useSettings hook

---
+++ src/hooks/settings.ts
@@ -0,0 +1,45 @@
+import { useEffect, useState } from "react";
+import { SettingsLocalStorage } from "../data/local";
+
+const settingsLocalStorage = new SettingsLocalStorage();
+
+export default function useSettings(userId: string) {
+  const [autoSave, setAutoSave] = useState(false);
+  const [useGrayscaleIcons, setUseGrayscaleIcons] = useState(false);
+
+  useEffect(() => {
+    const restoreSettings = async () => {
+      if (!userId) return;
+
+      const settingsContainer = await settingsLocalStorage.get(userId);
+      if (!settingsContainer) return;
+
+      const { settings } = settingsContainer;
+
+      if (settings) {
+        setAutoSave(settings.autoSave);
+        setUseGrayscaleIcons(settings.useGrayscaleIcons);
+      }
+    };
+
+    restoreSettings();
+  }, [userId]);
+
+  useEffect(() => {
+    const saveSettings = async () => {
+      if (!userId) return;
+
+      settingsLocalStorage.save({
+        id: userId,
+        settings: {
+          useGrayscaleIcons,
+          autoSave,
+        },
+      });
+    };
+
+    saveSettings();
+  }, [autoSave, useGrayscaleIcons]);
+
+  return [autoSave, setAutoSave, useGrayscaleIcons, setUseGrayscaleIcons] as const;
+}
